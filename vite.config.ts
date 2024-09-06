1065ec203162eeaf3b9ff6b18c60d3b4b353d73b
Jacob <simerly81@gmail.com>
2024-09-06 17:25:50 -0400

    ENV VARIABLE ARE STILL MISSING UGH

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
+console.log("Vite config environment variables:");
+console.log("VITE_API_URL:", process.env.VITE_API_URL);
+console.log("VITE_FRONTEND_URL:", process.env.VITE_FRONTEND_URL);
+console.log("VITE_FB_API_KEY:", process.env.VITE_FB_API_KEY);
+console.log("VITE_FB_AUTH_DOMAIN:", process.env.VITE_FB_AUTH_DOMAIN);
+console.log("VITE_PROJECT_ID:", process.env.VITE_PROJECT_ID);
+console.log("VITE_STORAGE_BUCKET:", process.env.VITE_STORAGE_BUCKET);
+console.log("VITE_MESSAGING_SENDER_ID:", process.env.VITE_MESSAGING_SENDER_ID);
+console.log("VITE_MEASUREMENT_ID:", process.env.VITE_MEASUREMENT_ID);
+
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   return {
