a4ac6de36f89fd537ff6deb8c906b05f6dfadf73
Jacob <simerly81@gmail.com>
2024-09-06 14:44:35 -0400

    updating format to try and start with cloud build

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,8 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), 'VITE_')
-  
-  return {
-    plugins: [react()],
-    define: {
-      'process.env': Object.keys(env).reduce((prev, key) => {
-        prev[key] = JSON.stringify(env[key]);
-        return prev;
-      }, {}),
-    },
-  }
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/',
+  plugins: [react()],
 })
